02a82acd4b6bd875f94020b83c86aee3af7591f7
michael.mcguirk <michael.mcguirk@workday.com>
2018-11-30 21:36:46 +0000

    server rendering

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    modules: [
+      path.resolve('./lib'),
+      path.resolve('./node_modules')
+    ]
+  },
+  entry: ['babel-polyfill', 'renderers/dom.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+    ]
+  }
+};
+
